379b3dbc7128035ff161ffc69b7e63d3ada2e8ec
sevas <f.degroef@gmail.com>
2014-03-21 21:39:28 +0100

    added setup of python-update-alternatives
    --HG--
    rename : config/fish/virtualenv.fish => config/fish/lib/virtualenv.fish

--- scripts/setup_new_system.sh
+++ scripts/setup_new_system.sh
@@ -1,14 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-push .
+pushd .
 cd ~
 
 mkdir -p .hgext
 cd .hgext
 hg clone https://bitbucket.org/sjl/hg-prompt
 
-cd ~
-cd .config
+
+
+popd
+pushd .
+mkdir -p ~/.config
+
+cp -r ../config/fish ~/.config
+
+cd ~/.config
 mkdir -p fish/lib
 cd fish/lib
 git clone https://github.com/sjl/z-fish
@@ -18,6 +25,17 @@ git clone https://github.com/sjl/z-fish
 cd ~
 cd .hgext
 hg clone https://bitbucket.org/sevas/mercurial-cli-templates
+
+
+
+cd ~
+mkdir -p build
+cd build
+hg clone https://bitbucket.org/sevas/python-update-alternatives
+
+cd python-update-alternatives
+
+python update_python_switchers.py
 
 
 popd
